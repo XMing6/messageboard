b5b20bd5b8da515918b3efec580654fa90a949f2
sammyliu(刘新明) <sammyliu@tencent.com>
2018-03-27 16:23:30 +0800

    1.修改controller

--- controllers/default.go
+++ controllers/default.go
@@ -13,3 +13,10 @@ func (c *MainController) Get() {
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
+
+func (c *MainController) Post() {
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+}
+ 
